01e6b73434951c9c3e92c69ead20d391d669492c
Vidya Tiwari <127404171+vidyatiwrai@users.noreply.github.com>
2023-08-04 16:20:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // write js code here if required
+function updateTimer() {
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const year = now.getFullYear();
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    const timerText = `${day}/${month}/${year}, ${hours}:${minutes}:${seconds}`;
+    document.getElementById("timer").textContent = timerText;
+}
+
+// Update the timer every second
+setInterval(updateTimer, 1000);
+
+// Initial call to display timer immediately
+updateTimer();
